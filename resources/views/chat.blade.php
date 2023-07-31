762fd80cd2a2d342556fdd381aa5d2f94dbeb2e3
krishnakumar <krishnakumar.m@g2techsoft.com>
2023-07-31 16:32:11 +0530

    updated file

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
 </head>
 
+
 <body>
     <section class="section">
         <div id="app" class="container">
